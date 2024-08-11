3c0640e6d8cda1a0038790c9b9e32d12ac16f6f2
djleo70 <83383164+djleo70@users.noreply.github.com>
2024-08-11 11:11:36 +0200

    Update MSI_Properties_Viewer.bat

--- MSI_Properties_Viewer.bat
+++ MSI_Properties_Viewer.bat
@@ -5,7 +5,7 @@
     set "Powershell_WindowStyle=Hidden"  :: Normal, Hidden, Minimized, Maximized
     set "Show_Loading=true"              :: Show cmd while preparing powershell
     set "Ensure_Local_Running=true"      :: If not launched from disk 'C', Re-Write in %temp% then execute
-        set "Show_Writing_Lines=true"    :: Show lines writing in %temp% while preparing powershell
+        set "Show_Writing_Lines=false"    :: Show lines writing in %temp% while preparing powershell
         set "Debug_Writting_Lines=false" :: Pause between each line writing (press a key to see next line)
     ::============================
 
